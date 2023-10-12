2bfdee5727f81ceacb550f601b0be0597c4d003a
1day2coffee <123rbdud@gmail.com>
2023-10-13 00:49:57 +0900

    update upload feed logic

--- content/views.py
+++ content/views.py
@@ -1,7 +1,11 @@
+import os
+from uuid import uuid4
+
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from content.models import Feed
+from instagram.settings import MEDIA_ROOT
 
 
 class Main(APIView):
@@ -11,17 +15,19 @@ class Main(APIView):
 
 class UploadFeed(APIView):
     def post(self, request):
-        file = request.data.get('file')
-        image = request.data.get('image')
+        file = request.FILES['file']
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
         content = request.data.get('content')
+        image = uuid_name
         profile_image = request.data.get('profile_image')
         user_id = request.data.get('user_id')
 
-
-        print(file)
-        print(image)
-        print(content)
-        print(profile_image)
-        print(user_id)
+        Feed.objects.create(content=content, image=image, profile_image=profile_image, user_id=user_id, like_count=0)
 
         return Response(status=200)
