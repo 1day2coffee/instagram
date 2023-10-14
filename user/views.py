12804b5594e3969e915ea15d935d6b42206de7a9
1day2coffee <123rbdud@gmail.com>
2023-10-14 13:25:23 +0900

    create join user back-end api

--- user/views.py
+++ user/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
 
 
 # Create your views here.
 class Join(APIView):
     def get(self, request):
         return render(request, "user/join.html")
+
+    def post(self, request):
+        email = request.data.get('email', None)
+        nickname = request.data.get('nickname', None)
+        name = request.data.get('name', None)
+        password = request.data.get('password', None)
+
+        User.objects.create(email=email, nickname=nickname, name=name, password=password)
+
+        return Response(status=200)
+
 
 class Login(APIView):
     def get(self, request):
