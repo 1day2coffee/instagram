7e237b9e2e038122b3d7ae82e71b681dc9798042
1day2coffee <123rbdud@gmail.com>
2023-10-17 15:27:01 +0900

    update user login, join logic

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import User
+from django.contrib.auth.hashers import make_password, check_password
 
 
 # Create your views here.
@@ -15,9 +16,14 @@ class Join(APIView):
         name = request.data.get('name', None)
         password = request.data.get('password', None)
 
-        User.objects.create(email=email, user_id=user_id, name=name, password=password)
+        if User.objects.filter(email=email).exists() :
+            return Response(status=500, data=dict(message='해당 이메일 주소가 존재합니다.'))
+        elif User.objects.filter(user_id=user_id).exists() :
+            return Response(status=500, data=dict(message='사용자 이름 "' + user_id + '"이(가) 존재합니다.'))
 
-        return Response(status=200)
+        User.objects.create(password=make_password(password),email=email, user_id=user_id, name=name)
+
+        return Response(status=200, data=dict(message="회원가입 성공했습니다. 로그인 해주세요."))
 
 
 class Login(APIView):
@@ -28,6 +34,21 @@ class Login(APIView):
         email = request.data.get('email', None)
         password = request.data.get('password', None)
 
-        User.objects.create(email=email, password=password)
+        if email is None:
+            return Response(status=500, data=dict(message='이메일을 입력해주세요'))
 
-        return Response(status=200)
+        if password is None:
+            return Response(status=500, data=dict(message='비밀번호를 입력해주세요'))
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return Response(status=500, data=dict(message='입력정보가 잘못되었습니다.'))
+
+        if check_password(password, user.password) is False:
+            return Response(status=500, data=dict(message='입력정보가 잘못되었습니다.'))
+
+        request.session['loginCheck'] = True
+        request.session['email'] = user.email
+
+        return Response(status=200, data=dict(message='로그인에 성공했습니다.'))
